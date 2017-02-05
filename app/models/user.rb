f307f2b7ced315fedbf77cfcddd371d52c08d2f9
Robert Gervais <email": null>
2017-02-05 15:09:18 -0600

    token creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ActiveRecord::Base
-  has_secure_token :auth_token
+ has_secure_password
+ has_secure_token :auth_token
+
+  def invalidate_token
+    self.update_columns(token: nil)
+  end
+
+  def self.valid_login?(email, password)
+    user = find_by(email: email)
+    if user && user.authenticate(password)
+      user
+    end
+  end
+  def with_unexpired_token(token, period)
+    where(token: token).where('token_created_at >= ?', period).first
+  end
+  def logout
+    invalidate_token
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  post 'user_token' => 'user_token#create'
   scope '/api' do
     scope '/v1' do
+      scope :format => true, :constraints => { :format => 'json' } do
+        post   "/login"       => "sessions#create"
+        delete "/logout"      => "sessions#destroy"
+      end
       get '/hello' => 'tests#hello'
       scope '/test' do
         resources :errands, :only => [:create, :index]
