2fc934a711df48638b1e2d3482cd5fd06b54eff7
Robert Gervais <email": null>
2017-02-07 11:46:11 -0600

    add user/create route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
 
   scope '/api' do
     scope '/v1' do
+      resources :users, :only => [:create]
       resources :errands, :except => [:show, :destroy, :new, :edit]
-        post   "/login"       => "sessions#create", :constraints => { :format => 'json' }
-        delete "/logout"      => "sessions#destroy", :constraints => { :format => 'json' }
+      post   "/login"       => "sessions#create", :constraints => { :format => 'json' }
+      delete "/logout"      => "sessions#destroy", :constraints => { :format => 'json' }
     end
   end
   get '/test', to: 'test#hello'
